4057d049801decd6909c563818f3a66dfe5ac30d
jeniffer-oliveira <jeniffer.campos.241201@gmail.com>
2022-08-19 22:18:29 -0300

    2 files modified

--- index.ts
+++ index.ts
@@ -1,13 +1,15 @@
 //8 ciclos com 20 seg de atividade e 10 seg de descanso
 
-function principalCounter() {
-  let firstCounter = 0;
-  firstCounter = firstCounter + 1;
+let round = 1
+let counter = 0
+let rest = 0 
+
+document.getElementById("round").innerHTML = "Round: " + round.toString()
+let intervalId = setInterval (() => {
+  counter += 1
+})
+
+if(counter <= 20 && rest == 0){
+  document.getElementById("set").innerHTML = "Start your exercise"
+  
 }
-
-//let interalId = setInterval(() => [
-//counter = counter + 1
-//let counterDiv = document.getElementById['app.counter'];
-//counterDiv.innerHTML = counter.toString[];
-
-//}, 1000;)
